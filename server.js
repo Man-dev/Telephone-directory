6c4e5a0ebba20f9460f8bfc4b1cc7293c95e5e54
Manmohan <manmohanverma111@gmail.com>
2018-06-11 14:48:51 +0530

    modified get/contacts/ method

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.get('/', function(req, res) {
 app.get('/contacts', function(req, res) {
 	db.contact.findAll().then(function(contacts) {
 		res.json(contacts);
+	}, function(e) {
+		res.json(e);
 	});
 });
 
@@ -39,25 +41,25 @@ app.put('/contact/:id', function(req, res) {
 	var body = _.pick(req.body, 'name', 'number');
 	var attributes = {};
 
-	if(body.hasOwnProperty('name')){
+	if (body.hasOwnProperty('name')) {
 		attributes.name = body.name;
 	}
 
-	if(body.hasOwnProperty('number')){
+	if (body.hasOwnProperty('number')) {
 		attributes.number = body.number;
 	}
 
 	db.contact.findById(id).then(function(contact) {
-		if(!contact){
+		if (!contact) {
 			res.status(400).send('Entry corresponding to queried id doesn\'t exist.');
 		} else {
-			contact.update(attributes).then(function(newContact){
+			contact.update(attributes).then(function(newContact) {
 				res.json(newContact);
-			}, function(e){
+			}, function(e) {
 				res.status(400).json(e);
 			});
 		}
-	}, function(e){
+	}, function(e) {
 		res.status(404).json(e);
 	});
 
